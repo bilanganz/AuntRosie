f0573b39472b32b4bd1060b96e06cda0ee73eafa
GabrielNathanLegawa <gabrielnathan.legawa@dcmail.ca>
2020-11-02 14:44:56 -0500

    Modify navbar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -20,18 +20,18 @@
       <div class="navbar navbar-dark bg-dark box-shadow">
         <div class="container d-flex justify-content-between">
           <a href="#" class="navbar-brand d-flex align-items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
-            <strong>Album</strong>
+            <img src="images/baker_icon.ico" alt="Aunt Rosie's Logo" width="45" height="45"/>
+            <strong>Aunt Rosie's Homemade Pies and Preserves</strong>
           </a>
           <div class="d-flex flex-row-reverse"">
                 <!-- Authentication Links -->
                 @guest
                        <div class="p-2">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        <a class="nav-link text-light font-weight-bold"  href="{{ route('login') }}">{{ __('Login') }}</a>
                        </div>
                     @if (Route::has('register'))
                        <div class="p-2">
-                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        <a class="nav-link text-light font-weight-bold" href="{{ route('register') }}">{{ __('Register') }}</a>
                        </div> 
                     @endif
                 @else

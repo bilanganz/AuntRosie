ab704d88286bef3273007670ebc63b15eadfeeca
GabrielNathanLegawa <gabrielnathan.legawa@dcmail.ca>
2020-11-02 15:08:23 -0500

    modify main content, add credit page.

---
+++ resources/views/credit.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('title', 'Credit')
+
+@section('content')
+<ul>
+<li>Gabriel Nathan Legawa</li>
+<li>Lisandra Legawa</li>
+<li>Ngoc Diep Nguyen</li>
+<li>Phillip Rogers</li>
+</ul>
+@stop
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -3,27 +3,36 @@
         <div class="container">
           <div class="row">
             <div class="col-sm-8 col-md-7 py-4">
-              <h4 class="text-white">About</h4>
-              <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
+              <h4 class="text-white">History</h4>
+              <p class="text-muted">
+                For the best in homemade meat pies and preserves the exacting and demanding
+                foodie need look no further than Aunt Rosie. Since the early 80’s, Rosie has been
+                making and selling her fare at a variety of farmers markets, home shows and bazaars
+                throughout southern Ontario. Over the last five years she has started to realize a
+                significant financial payoff for her years of perseverance and attention to quality in
+                everything she produces thanks to the developing “foodie” movement.
+              </p>
             </div>
             <div class="col-sm-4 offset-md-1 py-4">
               <h4 class="text-white">Contact</h4>
               <ul class="list-unstyled">
                 <li><a href="#" class="text-white">Follow on Twitter</a></li>
                 <li><a href="#" class="text-white">Like on Facebook</a></li>
+                <li><a href="#" class="text-white">Comment on Instagram</a></li>
                 <li><a href="#" class="text-white">Email me</a></li>
               </ul>
+              <h4 class="text-white"><a href="/credit" class="text-white">Credit</a></h4>
             </div>
           </div>
         </div>
       </div>
       <div class="navbar navbar-dark bg-dark box-shadow">
         <div class="container d-flex justify-content-between">
-          <a href="#" class="navbar-brand d-flex align-items-center">
+          <a href="/" class="navbar-brand d-flex align-items-center">
             <img src="images/baker_icon.ico" alt="Aunt Rosie's Logo" width="45" height="45"/>
             <strong>Aunt Rosie's Homemade Pies and Preserves</strong>
           </a>
-          <div class="d-flex flex-row-reverse"">
+          <div class="d-flex flex-row-reverse">
                 <!-- Authentication Links -->
                 @guest
                        <div class="p-2">

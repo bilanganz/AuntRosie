428e7c9e9ae4d55592dfa663c3c698368361200f
GabrielNathanLegawa <gabrielnathan.legawa@dcmail.ca>
2020-11-02 21:16:05 -0500

    Working on database portion.

--- database/migrations/2020_10_29_171548_create_medical_conditions_table.php
+++ database/migrations/2020_10_29_171548_create_medical_conditions_table.php
@@ -17,7 +17,6 @@ class CreateMedicalConditionsTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('description');
-            $table->timestamps();
         });
     }
 

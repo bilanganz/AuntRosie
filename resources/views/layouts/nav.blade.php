8d91d1ae6c9e18ab87003b3a4ae165d6bf099375
GabrielNathanLegawa <gabrielnathan.legawa@dcmail.ca>
2020-11-22 14:09:56 -0500

    modify nav.blade.php file.

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -16,21 +16,22 @@
                        <li><a class="nav-link text-light font-weight-bold" href="{{ route('register') }}">{{ __('Register') }}</a></li>
                     @endif
                 @else
-                        <div class="dropdown">
-                          <a id="navbarDropdown" class="nav-link dropdown-toggle text-light font-weight-bold" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                              {{ Auth::user()->name }}
-                          </a>
-                          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                  onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
-                                  {{ __('Logout') }}
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                          </div>
-                        </div>
+                  <li class="dropdown submenu">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button"
+                      aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</a>
+                    <ul class="dropdown-menu">
+                      <li class="dropdown-item">
+                       <a href="{{ route('logout') }}" class="text-primary"
+                           onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                          Sign Out
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="invisible">
+                      @csrf
+                  </form>
                 @endguest
           </ul>
           <!-- COMMENTED AT THE MOMENT FOR FUTURE DEVELOPMENT -->
@@ -40,11 +41,10 @@
         </div>
       </div>
     </div>
-    @auth
       <div class="main_menu_two">
         <div class="container">
           <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="/"><img src="img/logo.jpg" alt="" /></a>
+            <a class="navbar-brand" href="/"><img src="/img/logo.jpg" alt="" /></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
               aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="my_toggle_menu">
@@ -52,6 +52,7 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav justify-content-end">
+                @auth
                 <li class="active"><a href="/home">Home</a></li>
                 <li class="dropdown submenu">
                   <a class="dropdown-toggle" data-toggle="dropdown" href="recipe.html" role="button" aria-haspopup="true"
@@ -101,7 +102,6 @@
                     <li><a href="/sales/create">Add Sales</a></li>
                   </ul>
                 </li>
-                <li><a href="membership.html">Membership</a></li>
                 <li class="dropdown submenu">
                   <a class="dropdown-toggle" data-toggle="dropdown" href="customer.html" role="button" aria-haspopup="true"
                     aria-expanded="false">Customer</a>
@@ -110,10 +110,13 @@
                     <li><a href="/customers/create">Add Customer</a></li>
                   </ul>
                 </li>
+                @endauth
+                @guest
+                <li><a href="membership.html">Membership</a></li>
+                @endguest
               </ul>
             </div>
           </nav>
         </div>
     </div>
-  @endauth
 </header>
